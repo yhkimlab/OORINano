d338dcc2368b3ec8269a59c7bae1c309ae8adaf9
joonho <hopefulp@gmail.com>
2021-10-12 11:30:32 +0900

    modularize to top level

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 
 # Builders
 
-from . import build
-from . import catalysis
-
+#from . import build
+#from . import catalysis
+from .catalysis import vasp
+from .build     import surflab
