9ec8190ffc8c6eb57a15f5f839fc95e37f749da6
joonho <hopefulp@gmail.com>
2023-09-13 17:10:22 +0900

    modify for pdoc

--- NCexamples/bin/catalytic_info.py
+++ NCexamples/bin/catalytic_info.py
@@ -1,3 +1,32 @@
 import nanocore as nc
+import argparse
 
-nc.vasp.pdos_orbital_analysis(fname='SUM_ATOM.dat', orbitals=1)
+def post_process(job, inf, opt):
+    if job == 'pdos':
+        nc.vasp.pdos_orbital_analysis(fname=inf, orbitals=opt)
+    elif job == 'split':
+        at = nc.vasp.read_poscar(inf)       # in doscar_split.py
+        n_at = len(at)
+        nc.vasp.pdos_split_sum(sum_list=[i for i in range(n_at)])
+    return 0
+
+def main():
+    parser = argparse.ArgumentParser(description='Post process for VASP')
+    parser.add_argument('-j', '--job', choices=['pdos', 'split'], help='kind of post analysis')
+    parser.add_argument('-i', '--inf', default='SUM_ATOM.dat', choices=['SUM_ATOM.dat','POSCAR'], help='input file')
+    parser.add_argument('-o', '--option', default=1, type=int)
+    args = parser.parse_args()
+
+    pdos_analysis(args.job, args.inf, args.option)
+    return 0
+
+
+if __name__ == "__main__":
+    main()
+
+'''
+doscar_split.py
+at = nc.vasp.read_poscar('POSCAR')
+n_at = len(at)
+nc.vasp.pdos_split_sum(sum_list=[i for i in range(n_at)])
+'''
--- NCexamples/bin/doscar_split.py
+++ NCexamples/bin/doscar_split.py
@@ -1,6 +1,1 @@
-import nanocore as nc
-
-at = nc.vasp.read_poscar('POSCAR')
-n_at = len(at)
-nc.vasp.pdos_split_sum(sum_list=[i for i in range(n_at)])
-
+catalytic_info.py
